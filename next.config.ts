74034c1b630429b098672cb1824c0932a2b1b707
Dhruba Das <dhrubadas.dev@gmail.com>
2026-05-23 21:15:34 +0530

    Update Avatar Form & Update Name Form done

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   typedRoutes: true,
+
+  images: {
+    remotePatterns: [new URL("https://placehold.co/**")],
+  },
 };
 
 export default nextConfig;
